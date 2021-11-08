0d70575e154b7cca84a591c183da9b9ff5300cbc
Marcelo Rocha <marcelo.ds.rocha@outlook.com>
2021-11-08 20:24:32 -0300

    Enable/Disable Grid added

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-let slider = document.getElementById('pixels');
-let sliderNum = document.getElementById('pixelsValue');
+const slider = document.getElementById('pixels');
+const sliderNum = document.getElementById('pixelsValue');
+const mouseDetected = document.querySelector('body');
 let mousePressed = false;
-let clearButton = document.getElementById('clear');
-let sketchBackColorButton = document.getElementById('sketchBackColor');
+const clearButton = document.getElementById('clear');
+const sketchBackColorButton = document.getElementById('sketchBackColor');
+const gridButton = document.getElementById('gridButton');
+let gridState = true;
+
+
+gridButton.onclick = function(){
+    if(gridState){
+        gridState = false;
+    }else{
+        gridState = true;
+    }
+    gridStateFunc();
+}
 
 sketchBackColorButton.onchange = function(){
     const drawing = document.querySelector('.drawing');
@@ -18,6 +31,8 @@ slider.onmouseup = function(){
     recreateSketch(this.value);
 };
 
+//FUNCTIONS:
+
 //Creates a matrix with size X size, pixels for drawing.
 function createSketch(size){
     const drawing = document.querySelector('.drawing');
@@ -29,12 +44,14 @@ function createSketch(size){
             createPixel(drawing);
         }
     }
+
+    gridStateFunc();
 }
 
 //Create a single pixel and assign it to the DOM drawing.
 function createPixel(drawing){
     let pixel = document.createElement('div');
-    pixel.style.cssText = "border: 1px solid black; background: transparent;"
+    pixel.style.cssText = "background: transparent;"
     pixel.addEventListener('mousemove', changeColor);
     drawing.appendChild(pixel);
 }
@@ -49,6 +66,19 @@ function changeColor(){
     if(mousePressed){
         this.style.background = document.getElementById('pincel').value;
     }
+}
+
+function gridStateFunc(){
+    const pixel = document.querySelectorAll('.drawing > div');
+    if(gridState){
+        pixel.forEach((elem)=>{
+        elem.style.border = '1px solid black';
+    });
+    }else{
+        pixel.forEach((elem)=>{
+        elem.style.border = '';
+        });
+    }  
 }
 
 function clear(){
