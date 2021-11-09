cb9d6fde4db68ee8541a09c815e7fbabac4b8578
Marcelo Rocha <marcelo.ds.rocha@outlook.com>
2021-11-08 22:43:50 -0300

    Shading and Enlightening added

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ const clearButton = document.getElementById('clear');
 const sketchBackColorButton = document.getElementById('sketchBackColor');
 const gridButton = document.getElementById('gridButton');
 let gridState = true;
+const shadeButton = document.getElementById('Shade');
+const enlightButton = document.getElementById('Enlight');
+const normalButton = document.getElementById('Normal');
+let penState = 0;
 
+normalButton.onclick = ()=>{penState = 0;}
+shadeButton.onclick = function(){
+    penState = 2;
+}
+enlightButton.onclick = function(){
+    penState = 1;
+}
 
 gridButton.onclick = function(){
     if(gridState){
@@ -18,8 +29,10 @@ gridButton.onclick = function(){
 }
 
 sketchBackColorButton.onchange = function(){
-    const drawing = document.querySelector('.drawing');
-    drawing.style.background = this.value;
+    const pixel = document.querySelectorAll('.drawing > div');
+    pixel.forEach((elem)=>{
+        elem.style['background-color'] = document.getElementById('sketchBackColor').value;
+    });
 }
 
 clearButton.onclick = clear;
@@ -51,7 +64,7 @@ function createSketch(size){
 //Create a single pixel and assign it to the DOM drawing.
 function createPixel(drawing){
     let pixel = document.createElement('div');
-    pixel.style.cssText = "background: transparent;"
+    pixel.style['background-color'] = document.getElementById('sketchBackColor').value;
     pixel.addEventListener('mousemove', changeColor);
     drawing.appendChild(pixel);
 }
@@ -63,8 +76,32 @@ function recreateSketch(size){
 }
 
 function changeColor(){
+    let rgbColor;
+    let newRgbColor;
+    let colorString;
     if(mousePressed){
-        this.style.background = document.getElementById('pincel').value;
+        switch(penState){
+            case 0:
+                this.style['background-color'] = document.getElementById('pincel').value;
+                break;
+            case 1:
+                rgbColor = this.style['background-color'].slice(4,-1).split(', ');
+                newRgbColor = rgbColor.map((elem)=>{
+                    return Number(11*elem/10);
+                });
+                colorString = 'rgb('+newRgbColor.join(', ')+')';
+                this.style['background-color'] = colorString;
+                console.log('aaaa')
+                break;
+            case 2:
+                rgbColor = this.style['background-color'].slice(4,-1).split(', ');
+                newRgbColor = rgbColor.map((elem)=>{
+                    return Number(9*elem/10);
+                });
+                colorString = 'rgb('+newRgbColor.join(', ')+')';
+                this.style['background-color'] = colorString;
+                break;
+        }
     }
 }
 
@@ -84,7 +121,7 @@ function gridStateFunc(){
 function clear(){
     const pixel = document.querySelectorAll('.drawing > div');
     pixel.forEach((elem)=>{
-        elem.style.background = 'transparent';
+        elem.style['background-color'] = document.getElementById('sketchBackColor').value;
     });
 }
 
